5648fa6e61ffe427f6f59612d753e1034cba18a7
Watson Carawon <carawonw@gmail.com>
2025-10-26 22:41:02 -0400

    Finish basic raytracing
    This is kind of a mess but it works - except that the image is sideways...

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,24 @@
-use std::f32::INFINITY;
 use std::fs::File;
 use std::io::Write;
 use std::ops;
 
 struct Canvas {
-    height: i32,
-    width: i32,
+    height: f32,
+    width: f32,
 }
 
-impl Canvas {
-    fn new(height: i32, width: i32) -> Self {
-        Canvas { height, width }
-    }
-}
-
+#[derive(Clone, Copy, Debug, Default)]
 struct Color {
     r: f32,
     g: f32,
     b: f32,
 }
 
+#[derive(Clone, Copy, Debug, Default)]
 struct Vector {
     x: f32,
     y: f32,
     z: f32,
-}
-
-impl Vector {
-    fn new(x: f32, y: f32, z: f32) -> Self {
-        Vector { x, y, z }
-    }
 }
 
 impl ops::Add<Vector> for Vector {
@@ -49,9 +38,9 @@ impl ops::Sub<Vector> for Vector {
 
     fn sub(self, rhs: Vector) -> Self::Output {
         Vector {
-            x: self.x + rhs.x,
-            y: self.y + rhs.y,
-            z: self.z + rhs.z,
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
         }
     }
 }
@@ -84,28 +73,86 @@ fn dot(v: &Vector, w: &Vector) -> f32 {
     v.x * w.x + v.y * w.y + v.z * w.z
 }
 
+#[derive(Clone)]
 struct Sphere {
     center: Vector,
     radius: f32,
     color: Color,
 }
 
-const CANVAS: Canvas = Canvas {
-    height: 600,
-    width: 600,
-};
-const PROJECTION_PLANE_Z: i32 = 1; // distance from camera to projection plane
-const VIEWPORT_SIZE: i32 = 1;
+// globals
 
-fn canvas_to_viewport(x: i32, y: i32) -> Vector {
+const BACKGROUND_COLOR: Color = Color {
+    r: 255.0,
+    g: 255.0,
+    b: 255.0,
+};
+const CANVAS: Canvas = Canvas {
+    height: 600.0,
+    width: 600.0,
+};
+const CAMERA_POSITION: Vector = Vector {
+    x: 0.0,
+    y: 0.0,
+    z: 0.0,
+};
+const PROJECTION_PLANE_Z: f32 = 1.0; // distance from camera to projection plane
+const VIEWPORT_SIZE: f32 = 1.0;
+const SPHERES: [Sphere; 3] = [
+    Sphere {
+        center: Vector {
+            x: 0.0,
+            y: -1.0,
+            z: 3.0,
+        },
+        radius: 1.0,
+        color: Color {
+            r: 255.0,
+            g: 0.0,
+            b: 0.0,
+        },
+    },
+    Sphere {
+        center: Vector {
+            x: -2.0,
+            y: 0.0,
+            z: 4.0,
+        },
+        radius: 1.0,
+        color: Color {
+            r: 0.0,
+            g: 255.0,
+            b: 0.0,
+        },
+    },
+    Sphere {
+        center: Vector {
+            x: 2.0,
+            y: 0.0,
+            z: 4.0,
+        },
+        radius: 1.0,
+        color: Color {
+            r: 0.0,
+            g: 0.0,
+            b: 255.0,
+        },
+    },
+];
+
+fn canvas_to_viewport(x: f32, y: f32) -> Vector {
+    let a = x * VIEWPORT_SIZE / CANVAS.width;
+    let b = y * VIEWPORT_SIZE / CANVAS.height;
+    println!("{}, {}, {}, {}", CANVAS.width, CANVAS.height, a, b);
+
     Vector {
-        x: (x * VIEWPORT_SIZE / CANVAS.width) as f32,
-        y: (y * VIEWPORT_SIZE / CANVAS.height) as f32,
-        z: (PROJECTION_PLANE_Z) as f32,
+        x: (x * VIEWPORT_SIZE / CANVAS.width),
+        y: (y * VIEWPORT_SIZE / CANVAS.height),
+        z: (PROJECTION_PLANE_Z),
     }
 }
 
-fn intersect_ray_sphere(origin: Vector, direction: Vector, sphere: Sphere) -> Vec<f32> {
+fn intersect_ray_sphere(origin: Vector, direction: Vector, sphere: &Sphere) -> Vec<f32> {
     let r = sphere.radius;
     let co = origin - sphere.center;
 
@@ -113,28 +160,75 @@ fn intersect_ray_sphere(origin: Vector, direction: Vector, sphere: Sphere) -> Ve
     let b = 2.0 * dot(&co, &direction);
     let c = dot(&co, &co) - r * r;
 
+    println!("{:?}, {:?}, {}", co, direction, b);
+    // println!("{}, {}, {}", a, b, c);
+
     let discriminant = b * b - 4.0 * a * c;
     if discriminant < 0.0 {
-        return vec![INFINITY, INFINITY];
+        println!("discriminant < 0");
+        vec![f32::INFINITY, f32::INFINITY]
     } else {
-        let t1 = -b + f32::sqrt(discriminant) / 2.0 * a;
-        let t2 = -b - f32::sqrt(discriminant) / 2.0 * a;
-        return vec![t1, t2];
+        let t1 = (-b + f32::sqrt(discriminant)) / (2.0 * a);
+        let t2 = (-b - f32::sqrt(discriminant)) / (2.0 * a);
+        println!("{}, {}", t1, t2);
+        vec![t1, t2]
     }
 }
 
-fn put_pixel(x: f32, y: f32, color: Color) {
-    // TODO:
+fn trace_ray(origin: Vector, direction: Vector, t_min: f32, t_max: f32) -> Option<Color> {
+    let mut closest_t = f32::INFINITY;
+    let mut closest_sphere = None;
+
+    for sphere in SPHERES.iter() {
+        let ts = intersect_ray_sphere(origin, direction, sphere);
+        if (t_min < ts[0]) && (ts[0] < t_max) && (ts[0] < closest_t) {
+            closest_t = ts[0];
+            closest_sphere = Some(sphere);
+        } else if (t_min < ts[1]) && (ts[1] < t_max) && (ts[0] < closest_t) {
+            closest_t = ts[1];
+            closest_sphere = Some(sphere);
+        }
+    }
+
+    closest_sphere.map(|i| {
+        println!("{:?}", i.color);
+        i.color
+    })
+}
+
+fn put_pixel(x: f32, y: f32, color: &Option<Color>, file: &mut File) {
+    let x = CANVAS.width / 2.0 + x;
+    let y = CANVAS.height / 2.0 - y - 1.0;
+
+    if !(0.0..=CANVAS.width).contains(&x) || !(0.0..=CANVAS.height).contains(&y) {
+        return;
+    }
+
+    match color {
+        None => writeln!(
+            file,
+            "{}, {}, {}",
+            BACKGROUND_COLOR.r, BACKGROUND_COLOR.g, BACKGROUND_COLOR.b
+        )
+        .ok(),
+        Some(i) => writeln!(file, "{}, {}, {}", i.r, i.b, i.g).ok(),
+    };
 }
 
 fn main() {
     let mut file = File::create("image.ppm").expect("Unable to create file");
 
-    for x in (-CANVAS.width / 2)..(CANVAS.width / 2) {
-        for y in (-CANVAS.height / 2)..(CANVAS.height / 2) {
-            let direction = canvas_to_viewport(x, y);
+    writeln!(file, "P3").ok();
+    writeln!(file, "{} {}", CANVAS.width, CANVAS.height).ok();
+    writeln!(file, "255").ok();
 
-            file.write_all(b"").expect("Unable to write data")
+    for x in (-CANVAS.width as i32 / 2)..(CANVAS.width as i32 / 2 + 1) {
+        for y in (-CANVAS.height as i32 / 2)..(CANVAS.height as i32 / 2 + 1) {
+            let direction = canvas_to_viewport(x as f32, y as f32);
+            println!("{:?}", direction);
+            let color = trace_ray(CAMERA_POSITION, direction, 1.0, f32::INFINITY);
+
+            put_pixel(x as f32, y as f32, &color, &mut file);
         }
     }
 }
