96167708da836f733ef299758a2ab7ae9b3979d3
Watson Carawon <carawonw@gmail.com>
2025-10-31 13:48:40 -0400

    Remove debug prints and clean up error handling

--- src/main.rs
+++ src/main.rs
@@ -141,13 +141,11 @@ const SPHERES: [Sphere; 3] = [
 ];
 
 fn canvas_to_viewport(x: f32, y: f32) -> Vector {
-    let a = x * VIEWPORT_SIZE / CANVAS.width;
-    let b = y * VIEWPORT_SIZE / CANVAS.height;
-    println!("{}, {}, {}, {}", CANVAS.width, CANVAS.height, a, b);
-
+    // I am not sure why I need to invert the y axis
+    // NOTE: Consider removing inversion later if another output format corrects it
     Vector {
         x: (x * VIEWPORT_SIZE / CANVAS.width),
-        y: (y * VIEWPORT_SIZE / CANVAS.height),
+        y: -(y * VIEWPORT_SIZE / CANVAS.height),
         z: (PROJECTION_PLANE_Z),
     }
 }
@@ -160,17 +158,17 @@ fn intersect_ray_sphere(origin: Vector, direction: Vector, sphere: &Sphere) -> V
     let b = 2.0 * dot(&co, &direction);
     let c = dot(&co, &co) - r * r;
 
-    println!("{:?}, {:?}, {}", co, direction, b);
+    // println!("{:?}, {:?}, {}", co, direction, b);
     // println!("{}, {}, {}", a, b, c);
 
     let discriminant = b * b - 4.0 * a * c;
     if discriminant < 0.0 {
-        println!("discriminant < 0");
+        // println!("discriminant < 0");
         vec![f32::INFINITY, f32::INFINITY]
     } else {
         let t1 = (-b + f32::sqrt(discriminant)) / (2.0 * a);
         let t2 = (-b - f32::sqrt(discriminant)) / (2.0 * a);
-        println!("{}, {}", t1, t2);
+        // println!("{}, {}", t1, t2);
         vec![t1, t2]
     }
 }
@@ -184,14 +182,14 @@ fn trace_ray(origin: Vector, direction: Vector, t_min: f32, t_max: f32) -> Optio
         if (t_min < ts[0]) && (ts[0] < t_max) && (ts[0] < closest_t) {
             closest_t = ts[0];
             closest_sphere = Some(sphere);
-        } else if (t_min < ts[1]) && (ts[1] < t_max) && (ts[0] < closest_t) {
+        } else if (t_min < ts[1]) && (ts[1] < t_max) && (ts[1] < closest_t) {
             closest_t = ts[1];
             closest_sphere = Some(sphere);
         }
     }
 
     closest_sphere.map(|i| {
-        println!("{:?}", i.color);
+        // println!("{:?}", i.color);
         i.color
     })
 }
@@ -210,22 +208,28 @@ fn put_pixel(x: f32, y: f32, color: &Option<Color>, file: &mut File) {
             "{}, {}, {}",
             BACKGROUND_COLOR.r, BACKGROUND_COLOR.g, BACKGROUND_COLOR.b
         )
-        .ok(),
-        Some(i) => writeln!(file, "{}, {}, {}", i.r, i.b, i.g).ok(),
+        .expect("Error writing background color to file."),
+        Some(i) => {
+            writeln!(file, "{}, {}, {}", i.r, i.g, i.b).expect("Error writing color to file.")
+        }
     };
 }
 
 fn main() {
     let mut file = File::create("image.ppm").expect("Unable to create file");
 
-    writeln!(file, "P3").ok();
-    writeln!(file, "{} {}", CANVAS.width, CANVAS.height).ok();
-    writeln!(file, "255").ok();
+    writeln!(file, "P3\n{} {}\n255", CANVAS.width, CANVAS.height)
+        .expect("Error writing header to file.");
 
-    for x in (-CANVAS.width as i32 / 2)..(CANVAS.width as i32 / 2 + 1) {
-        for y in (-CANVAS.height as i32 / 2)..(CANVAS.height as i32 / 2 + 1) {
+    let min_width = -(CANVAS.width / 2.0) as i32;
+    let max_width = (CANVAS.width / 2.0) as i32;
+    let min_height = -(CANVAS.height / 2.0) as i32;
+    let max_height = (CANVAS.height / 2.0) as i32;
+
+    for y in (min_height)..(max_height) {
+        for x in (min_width)..(max_width) {
             let direction = canvas_to_viewport(x as f32, y as f32);
-            println!("{:?}", direction);
+            // println!("{:?}", direction);
             let color = trace_ray(CAMERA_POSITION, direction, 1.0, f32::INFINITY);
 
             put_pixel(x as f32, y as f32, &color, &mut file);
